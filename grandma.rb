703aab8e61d9b3da3fd833f132ead0ad43c70f99
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-12 14:26:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,12 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(phrase)
+  if(phrase.upcase)
+    puts 'I LOVE YOU TOO PUMPKIN!'
+  elsif (!phrase.upcase)
+  puts 'HUH?! SPEAK UP, SONNY!'
+  elsif (phrase.upcase && ((phrase.length - 1) == "?"))
+  puts 'NO, NOT SINCE 1938!'
+  end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
